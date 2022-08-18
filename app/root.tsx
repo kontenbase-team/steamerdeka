e254b9819fd9f9a03ce3ce42b3811883e63ec9d3
M Haidar Hanif <git@mhaidarhanif.com>
2022-08-18 20:24:53 +0700

    create remix app

---
+++ app/root.tsx
@@ -0,0 +1,33 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  viewport: "width=device-width,initial-scale=1",
+  title: "Steamerdeka",
+  description: "Steamerdeka is a Steam clone with Indonesian vibe.",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Steamerdeka</h1>
+    </div>
+  );
+}
